b412fbe53e05dbe04d1ee277c3d96864140f4701
gusplayer <fosterstudios.colombia@gmail.com>
2017-05-18 19:22:03 -0500

    Conectado a la API y con todas las librerias y permisos

---
+++ app.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import Home from './home';
+import Fail from './fail';
+import Succes from './succes';
+
+import {Scene, Router} from 'react-native-router-flux';
+
+
+
+export default class Led3 extends Component {
+
+  render() {
+    return(
+    <Router>
+      <Scene key="root">
+        <Scene key="home" component={Home} title="Miràà"/>
+        <Scene key="fail" component={Fail} title="Incorrecto"/>
+        <Scene key="correcto" component={Succes} title="Contenido" hideNavBar={true}/>
+      </Scene>
+    </Router>
+    )
+  }
+
+}//end class
+
+AppRegistry.registerComponent('Led3', () => Led3);
---
+++ succes.js
@@ -0,0 +1,37 @@
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class fail extends Component {
+
+  render(){
+    return(
+      <View style={styles.container}>
+         <Text style={styles.mensaje}>Aqui esta correcto</Text>
+      </View>
+    )
+  }
+
+}
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'ghostwhite'
+  },
+  mensaje:{
+    color:'blue',
+    fontSize:24,
+    justifyContent:'space-around',
+    padding:20,
+    textAlign:'center'
+  }
+
+});
